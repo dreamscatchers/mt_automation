c3f16d95ed73c719493f935b3725fef26a2d5fe7
yyk <yyk@mail.ru>
2025-12-16 13:36:21 -0400

    feat: name/description builers added

---
+++ gas/mtm_content.js
@@ -0,0 +1,10 @@
+function buildStreamTitle_(index) {
+  return index + ". Master's Touch Meditation — Day " + index + " of 1000";
+}
+
+function buildStreamDescription_() {
+  return "#YogiBhajan #Meditation #Sadhana #DailyPractice #1000DaysChallenge " +
+         "#MastersTouchMeditation #KundaliniYoga #MeditationJourney " +
+         "#SpiritualDiscipline #MeditationChallenge #DailyMeditation " +
+         "#LongMeditation #MeditationSadhana #YogaPractice #MeditationLife";
+}
