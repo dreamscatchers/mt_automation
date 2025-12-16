771215183c1ed9286b82789cdd6dae580081a76e
yyk <yyk@mail.ru>
2025-12-16 13:34:20 -0400

    ref: thumb folder id added to config

--- gas/config.js
+++ gas/config.js
@@ -1,3 +1,4 @@
 const CHANNEL_ID = 'UCMqd1VhAkxcGC6AZRQW2ZmA';
 const DEFAULT_TZ = 'America/Santo_Domingo';
 const BASE_DATE_YMD = '2025-02-20';
+const THUMB_FOLDER_ID = "1hiiAL0X_YchX56UdZbBX65uWgRhTGEZv";
