6b765cd9a6eed5c6eb1f7749083ba1c84529cbc5
yyk <yyk@mail.ru>
2025-12-16 14:33:55 -0400

    fix: actual trigger code added

---
+++ gas/trigger.js
@@ -0,0 +1,20 @@
+function runProcessBackupVideoTrigger() {
+  try {
+    var day = Utilities.formatDate(
+      new Date(),
+      Session.getScriptTimeZone(),
+      'yyyy-MM-dd'
+    );
+
+    var res = processBackupVideoForDay_(day, {
+      maxPages: 3,
+      dryRun: false,
+      verbose: false
+    });
+
+    Logger.log(JSON.stringify(res, null, 2));
+  } catch (e) {
+    console.error('[runProcessBackupVideoTrigger]', e);
+    throw e;
+  }
+}
